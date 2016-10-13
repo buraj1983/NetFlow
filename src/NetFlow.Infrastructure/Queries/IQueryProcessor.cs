fd008473828ed291a464f303a6119972fab19c57
Dawid Buras <dawid.buras@gmail.com>
2016-10-13 19:49:35 +0200

    Fix

---
+++ src/NetFlow.Infrastructure/Queries/IQueryProcessor.cs
@@ -0,0 +1,12 @@
+﻿namespace NetFlow.Infrastructure.Queries
+{
+    public interface IQueryProcessor
+    {
+        TResult Process<TQuery, TResult>(TQuery query) 
+            where TQuery : IQuery<TResult>;
+
+        void RegisterHandler<TQueryHandler, TQuery, TResult>() 
+            where TQueryHandler : IQueryHandler<TQuery, TResult>
+            where TQuery : IQuery<TResult>;
+    }
+}
